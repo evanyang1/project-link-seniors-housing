24781f62a786fa0c995bc4c18b9b44837d101fc6
Evan Yang <evanjyang4@gmail.com>
2025-06-13 17:53:51 -0700

    tanstack

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import './App.css'
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
 
 function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
